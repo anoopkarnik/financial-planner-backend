aff3256e9af0d8d18f9706b926cc687f49d2bd60
anoopkarnik <danoopkarnik@gmail.com>
2022-12-09 18:14:37 +0530

    first commit

---
+++ expenses.js
@@ -0,0 +1,397 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+    user: 'anoop',
+    host: 'localhost',
+    database: 'finances',
+    password: 'password',
+    port: 5433
+})
+
+const getUnplannedExpenses = (body,start_time,end_time) => {
+    return new Promise(function(resolve,reject){
+        const {expense_types,account_types,category_types} = body;
+        let expense_type_query = "";
+        for (let i =0;i<expense_types.length;i++){
+            expense_type_query+="'"+expense_types[i]+"',"
+        }
+        expense_type_query = expense_type_query.substring(0,expense_type_query.length-1)
+        let account_type_query = "";
+        for (let i =0;i<account_types.length;i++){
+            account_type_query+="'"+account_types[i]+"',"
+        }
+        account_type_query = account_type_query.substring(0,account_type_query.length-1)
+        let category_type_query = "";
+        for (let i =0;i<category_types.length;i++){
+            category_type_query+="'"+category_types[i]+"',"
+        }
+        category_type_query = category_type_query.substring(0,category_type_query.length-1)
+        const query = "Select * from budget.unplanned_expenses where active = true and created_at>='"+String(start_time)+"' and created_at<'"+String(end_time)+"' and expense_type in ("+expense_type_query+") and account_type in ("+account_type_query+") and category_type in ("+category_type_query+") order by created_at asc"
+        pool.query(query,(error,results)=>{
+            if (error) {
+                reject(error)
+            }
+            resolve(results.rows);
+        })
+    })
+}
+
+const getMonthlyPlannedExpenses = (body,start_time,end_time) => {
+    return new Promise(function(resolve,reject){
+        const {expense_types,account_types,category_types} = body;
+        let expense_type_query = "";
+        for (let i =0;i<expense_types.length;i++){
+            expense_type_query+="'"+expense_types[i]+"',"
+        }
+        expense_type_query = expense_type_query.substring(0,expense_type_query.length-1)
+        let account_type_query = "";
+        for (let i =0;i<account_types.length;i++){
+            account_type_query+="'"+account_types[i]+"',"
+        }
+        account_type_query = account_type_query.substring(0,account_type_query.length-1)
+        let category_type_query = "";
+        for (let i =0;i<category_types.length;i++){
+            category_type_query+="'"+category_types[i]+"',"
+        }
+        category_type_query = category_type_query.substring(0,category_type_query.length-1)
+        const query = "Select * from budget.monthly_expenses where active = true and created_at>='"+String(start_time)+"' and created_at<'"+String(end_time)+"' and expense_type in ("+expense_type_query+") and account_type in ("+account_type_query+") and category_type in ("+category_type_query+")"
+        pool.query(query,(error,results)=>{
+            if (error) {
+                reject(error)
+            }
+            resolve(results.rows);
+        })
+    })
+}
+
+const getTotalUnplannedExpense = (start_time,end_time) =>{
+    return new Promise(function(resolve,reject){
+        const query = "Select sum(cost) from budget.unplanned_expenses where active = true and created_at>='"+String(start_time)+"' and created_at<'"+String(end_time)+"'"
+        pool.query(query,(error,results) =>{
+            if (error) {
+                reject(error)
+            }
+            resolve(results.rows[0]);
+        })
+    })
+}
+
+const getTotalMonthlyPlannedExpense = (start_time,end_time) =>{
+    return new Promise(function(resolve,reject){
+        const query = "Select sum(cost) from budget.monthly_expenses where active = true  and created_at>='"+String(start_time)+"' and created_at<'"+String(end_time)+"'"
+        pool.query(query,(error,results) =>{
+            if (error) {
+                reject(error)
+            }
+            resolve(results.rows[0]);
+        })
+    })
+}
+
+const getTotalIncome = (start_time,end_time) =>{
+    return new Promise(function(resolve,reject){
+        const query = "Select sum(cost) from budget.incomes where active = true"
+        pool.query(query,(error,results) =>{
+            if (error) {
+                reject(error)
+            }
+            resolve(results.rows[0]);
+        })
+    })
+}
+
+const getRemaining = (start_time,end_time) =>{
+    return new Promise(function(resolve,reject){
+        const query = "select ((Select sum(cost) from budget.incomes where active = true)- coalesce((Select sum(cost) from budget.unplanned_expenses where active = true and created_at>='"+String(start_time)+"' and created_at<'"+String(end_time)+"'),0) -  coalesce((Select sum(cost) from budget.monthly_expenses where active = true and created_at>='"+String(start_time)+"' and created_at<'"+String(end_time)+"'),0)) sum"
+        pool.query(query,(error,results) =>{
+            if (error) {
+                reject(error)
+            }
+            resolve(results.rows[0]);
+        })
+    })
+}
+
+const getMonths = () =>{
+    return new Promise(function(resolve,reject){
+        pool.query('select * from common.current_month order by start_time',(error,results) =>{
+            if (error) {
+                reject(error)
+            }
+            resolve(results.rows);
+        })
+    })
+}
+
+const getExpenseType = () =>{
+    return new Promise(function(resolve,reject){
+        pool.query('select * from common.expense_type order by id',(error,results) =>{
+            if (error) {
+                reject(error)
+            }
+            resolve(results.rows);
+        })
+    })
+}
+
+const getCategories = (expense_type) =>{
+    return new Promise(function(resolve,reject){
+        const query = 'select * from common.category_type where expense_type_id =' +String(expense_type) +' order by id'
+        pool.query(query,(error,results) =>{
+            if (error) {
+                reject(error)
+            }
+            resolve(results.rows);
+        })
+    })
+}
+
+const getAllCategories = (expense_type) =>{
+    return new Promise(function(resolve,reject){
+        const query = 'select * from common.category_type order by id'
+        pool.query(query,(error,results) =>{
+            if (error) {
+                reject(error)
+            }
+            resolve(results.rows);
+        })
+    })
+}
+
+const getSubcategories = (category_type) =>{
+    return new Promise(function(resolve,reject){
+        const query = 'select * from common.sub_category_type where category_id =' +String(category_type) +' order by id'
+        pool.query(query,(error,results) =>{
+            if (error) {
+                reject(error)
+            }
+            resolve(results.rows);
+        })
+    })
+}
+
+const getAccounts = () =>{
+    return new Promise(function(resolve,reject){
+        const query = 'select * from common.account_type order by id'
+        pool.query(query,(error,results) =>{
+            if (error) {
+                reject(error)
+            }
+            resolve(results.rows);
+        })
+    })
+}
+
+const getSubaccounts= (account_type) =>{
+    return new Promise(function(resolve,reject){
+        const query = 'select * from common.sub_account_type where account_id =' +String(account_type) +' order by id'
+        pool.query(query,(error,results) =>{
+            if (error) {
+                reject(error)
+            }
+            resolve(results.rows);
+        })
+    })
+}
+
+
+
+const getMonthRange = (id) =>{
+    return new Promise(function(resolve,reject){
+        pool.query('select * from common.current_month where id = $1',[id],(error,results) =>{
+            if (error) {
+                reject(error)
+            }
+            resolve(results.rows[0]);
+        })
+    })
+}
+
+const createExpense = (body) =>{
+    return new Promise(function(resolve,reject){
+        const {name,cost,expense_type,category_type,sub_category_type,account_type,sub_account_type} = body
+        pool.query('Insert into budget.unplanned_expenses(name,cost,expense_type,category_type,sub_category_type,account_type,sub_account_type) values ($1,$2,$3,$4,$5,$6,$7) returning *',[name,cost,expense_type,category_type,sub_category_type,account_type,sub_account_type],(error,results) =>{
+            if (error) {
+                reject(error)
+            }
+            resolve(`A new expense has been added : ${results.rows[0]}`);
+        })
+    })
+}
+
+const createMonthlyExpense = (body) =>{
+    return new Promise(function(resolve,reject){
+        const {name,cost,expense_type,category_type,sub_category_type,account_type,sub_account_type} = body
+        pool.query('Insert into budget.monthly_expenses(name,cost,expense_type,category_type,sub_category_type,account_type,sub_account_type) values ($1,$2,$3,$4,$5,$6,$7) returning *',[name,cost,expense_type,category_type,sub_category_type,account_type,sub_account_type],(error,results) =>{
+            if (error) {
+                reject(error)
+            }
+            resolve(`A new expense has been added : ${results.rows[0]}`);
+        })
+    })
+}
+
+const updateAccountBalance = (body) =>{
+    return new Promise(function(resolve,reject){
+        const {cost,sub_account_type} = body
+        pool.query('update common.sub_account_type set balance = balance - $1 where id = $2',[cost,sub_account_type],(error,results) =>{
+            if (error) {
+                reject(error)
+            }
+            resolve(`Account updated : ${results}`);
+        })
+    })
+}
+
+const deleteExpense = (request,response) => {
+    const id = parseInt(request.params.id)
+    pool.query('update budget.unplanned_expenses set active = false where id = $1',[id],(error,results)=>{
+        if (error){
+           throw error
+        }
+        response.status(200).send(`User deleted withh ID: ${id}`)
+    })
+}
+
+const deleteMonthlyExpense = (request,response) => {
+    const id = parseInt(request.params.id)
+    pool.query('update budget.monthly_expenses set active = false where id = $1',[id],(error,results)=>{
+        if (error){
+           throw error
+        }
+        response.status(200).send(`User deleted withh ID: ${id}`)
+    })
+}
+
+const deleteMonthlyBudget = (request,response) => {
+    const id = parseInt(request.params.id)
+    pool.query('update budget.monthly_budget set active = false where id = $1',[id],(error,results)=>{
+        if (error){
+           throw error
+        }
+        response.status(200).send(`Budget payed with ID: ${id}`)
+    })
+}
+
+const getAllBalances = (request,response) => {
+    return new Promise(function(resolve,reject){
+        const query = 'Select * from common.sub_account_type where balance!=0 order by id'
+        pool.query(query,(error,results)=>{
+            if (error) {
+                reject(error)
+            }
+            resolve(results.rows);
+        })
+    })
+}
+
+const getTotalBalance = (request,response) => {
+    return new Promise(function(resolve,reject){
+        pool.query('Select sum(balance) from common.sub_account_type',(error,results)=>{
+            if (error){
+                throw error
+            }
+            resolve(results.rows[0]);
+        })
+    })
+}
+
+const getLiquidityBalance= (request,response) => {
+    return new Promise(function(resolve,reject){
+        pool.query('Select sum(balance) from common.sub_account_type where liquidity = true',(error,results)=>{
+            if (error){
+                throw error
+            }
+            resolve(results.rows[0]);
+        })
+    })
+}
+
+const getFreeLiquidityBalance = (request,response) => {
+    return new Promise(function(resolve,reject){
+        pool.query('Select sum(balance) from common.sub_account_type where free_liquidity = true',(error,results)=>{
+            if (error){
+                throw error
+            }
+            resolve(results.rows[0]);
+        })
+    })
+}
+
+const getBudget= (expense_type) => {
+    return new Promise(function(resolve,reject){
+        const query = "Select coalesce(sum(cost),0) sum from budget.monthly_budget where expense_type = '" +expense_type +"'"
+        pool.query(query,(error,results)=>{
+            if (error){
+                throw error
+            }
+            resolve(results.rows[0]);
+        })
+    })
+}
+
+const getTotalBudget= (expense_type) => {
+    return new Promise(function(resolve,reject){
+        const query = "Select plan_percentage from budget.monthly_budget_plan where expense_type = '" +expense_type +"'"
+        pool.query(query,(error,results)=>{
+            if (error){
+                throw error
+            }
+            resolve(results.rows[0]);
+        })
+    })
+}
+
+const getAllBudget= (expense_type) => {
+    return new Promise(function(resolve,reject){
+        const query = "Select * from budget.monthly_budget where expense_type = '" +expense_type +"'"
+        pool.query(query,(error,results)=>{
+            if (error){
+                throw error
+            }
+            resolve(results.rows);
+        })
+    })
+}
+
+const getTransactions = (expense_type) => {
+    return new Promise(function(resolve,reject){
+        const query = "Select (Select coalesce(sum(cost),0) sum from budget.monthly_expenses where active = true and expense_type = '" +expense_type +"') + (Select coalesce(sum(cost),0) sum from budget.unplanned_expenses where active = true and expense_type = '" +expense_type +"') sum"
+        pool.query(query,(error,results)=>{
+            if (error){
+                throw error
+            }
+            resolve(results.rows[0]);
+        })
+    })
+}
+
+module.exports = {
+    getUnplannedExpenses,
+    getTotalUnplannedExpense,
+    getTotalMonthlyPlannedExpense,
+    getTotalIncome,
+    createExpense,
+    createMonthlyExpense,
+    deleteExpense,
+    getRemaining,
+    getMonths,
+    getMonthRange,
+    getExpenseType,
+    getMonthlyPlannedExpenses,
+    getCategories,
+    getSubcategories,
+    getAccounts,
+    getSubaccounts,
+    getAllCategories,
+    getAllBalances,
+    getTotalBalance,
+    getLiquidityBalance,
+    getFreeLiquidityBalance,
+    deleteMonthlyExpense,
+    updateAccountBalance,
+    getBudget,
+    getAllBudget,
+    getTransactions,
+    deleteMonthlyBudget ,
+    getTotalBudget
+}
---
+++ index.js
@@ -0,0 +1,276 @@
+const express = require('express')
+const app = express()
+const port = 3002
+
+const expenses = require('./expenses')
+
+app.use(express.json())
+
+app.use(function(req,res,next){
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+    next();
+})
+
+app.post('/unplannedExpenses', (req,res)=>{
+    expenses.getUnplannedExpenses(req.body,req.query.start_time,req.query.end_time)
+    .then(response=>{
+        res.status(200).send(response);
+    })
+    .catch(error=>{
+        res.status(500).send(error);
+    })
+})
+
+app.post('/monthlyPlannedExpenses', (req,res)=>{
+    expenses.getMonthlyPlannedExpenses(req.body,req.query.start_time,req.query.end_time)
+    .then(response=>{
+        res.status(200).send(response);
+    })
+    .catch(error=>{
+        res.status(500).send(error);
+    })
+})
+
+app.get('/getBalances', (req,res)=>{
+    expenses.getAllBalances()
+    .then(response=>{
+        res.status(200).send(response);
+    })
+    .catch(error=>{
+        res.status(500).send(error);
+    })
+})
+
+app.get('/months',(req,res) =>{
+    expenses.getMonths()
+    .then(response=>{
+        res.status(200).send(response);
+    })
+    .catch(error=>{
+        res.status(500).send(error);
+    })
+})
+
+app.get('/expenseType',(req,res) =>{
+    expenses.getExpenseType()
+    .then(response=>{
+        res.status(200).send(response);
+    })
+    .catch(error=>{
+        res.status(500).send(error);
+    })
+})
+
+app.get('/categoryTypes',(req,res) =>{
+    expenses.getCategories(req.query.expense_type)
+    .then(response=>{
+        res.status(200).send(response);
+    })
+    .catch(error=>{
+        res.status(500).send(error);
+    })
+})
+
+app.get('/allCategoryTypes',(req,res) =>{
+    expenses.getAllCategories()
+    .then(response=>{
+        res.status(200).send(response);
+    })
+    .catch(error=>{
+        res.status(500).send(error);
+    })
+})
+
+app.get('/subcategoryTypes',(req,res) =>{
+    expenses.getSubcategories(req.query.category_type)
+    .then(response=>{
+        res.status(200).send(response);
+    })
+    .catch(error=>{
+        res.status(500).send(error);
+    })
+})
+
+
+app.get('/accountTypes',(req,res) =>{
+    expenses.getAccounts()
+    .then(response=>{
+        res.status(200).send(response);
+    })
+    .catch(error=>{
+        res.status(500).send(error);
+    })
+})
+
+app.get('/subaccountTypes',(req,res) =>{
+    expenses.getSubaccounts(req.query.account_type)
+    .then(response=>{
+        res.status(200).send(response);
+    })
+    .catch(error=>{
+        res.status(500).send(error);
+    })
+})
+
+
+app.get('/month',(req,res) =>{
+    expenses.getMonthRange(req.query.id)
+    .then(response=>{
+        res.status(200).send(response);
+    })
+    .catch(error=>{
+        res.status(500).send(error);
+    })
+})
+
+
+app.get('/totalincome',(req,res) =>{
+    expenses.getTotalIncome(req.query.start_time,req.query.end_time)
+    .then(response=>{
+        res.status(200).send(response);
+    })
+    .catch(error=>{
+        res.status(500).send(error);
+    })
+})
+
+app.get('/totalunplannedexpense',(req,res) =>{
+    expenses.getTotalUnplannedExpense(req.query.start_time,req.query.end_time)
+    .then(response=>{
+        res.status(200).send(response);
+    })
+    .catch(error=>{
+        res.status(500).send(error);
+    })
+})
+
+app.get('/totalmonthlyplannedexpense',(req,res) =>{
+    expenses.getTotalMonthlyPlannedExpense(req.query.start_time,req.query.end_time)
+    .then(response=>{
+        res.status(200).send(response);
+    })
+    .catch(error=>{
+        res.status(500).send(error);
+    })
+})
+
+
+app.get('/remaining',(req,res) =>{
+    expenses.getRemaining(req.query.start_time,req.query.end_time)
+    .then(response=>{
+        res.status(200).send(response);
+    })
+    .catch(error=>{
+        res.status(500).send(error);
+    })
+})
+
+app.get('/getTotalBalance', (req,res)=>{
+    expenses.getTotalBalance()
+    .then(response=>{
+        res.status(200).send(response);
+    })
+    .catch(error=>{
+        res.status(500).send(error);
+    })
+})
+
+app.get('/getLiquidityBalance', (req,res)=>{
+    expenses.getLiquidityBalance()
+    .then(response=>{
+        res.status(200).send(response);
+    })
+    .catch(error=>{
+        res.status(500).send(error);
+    })
+})
+
+app.get('/getFreeLiquidityBalance', (req,res)=>{
+    expenses.getFreeLiquidityBalance()
+    .then(response=>{
+        res.status(200).send(response);
+    })
+    .catch(error=>{
+        res.status(500).send(error);
+    })
+})
+
+app.post('/expense',(req,res) =>{
+    expenses.createExpense(req.body)
+    .then(response=>{
+        res.status(200).send(response);
+    })
+    .catch(error=>{
+        res.status(500).send(error);
+    })
+})
+
+app.post('/monthlyExpense',(req,res) =>{
+    expenses.createMonthlyExpense(req.body)
+    .then(response=>{
+        res.status(200).send(response);
+    })
+    .catch(error=>{
+        res.status(500).send(error);
+    })
+})
+
+app.post('/updateBalance',(req,res) =>{
+    expenses.updateAccountBalance(req.body)
+    .then(response=>{
+        res.status(200).send(response);
+    })
+    .catch(error=>{
+        res.status(500).send(error);
+    })
+})
+
+app.get('/getBudget',(req,res) =>{
+    expenses.getBudget(req.query.expense_type)
+    .then(response=>{
+        res.status(200).send(response);
+    })
+    .catch(error=>{
+        res.status(500).send(error);
+    })
+})
+
+app.get('/getAllBudget',(req,res) =>{
+    expenses.getAllBudget(req.query.expense_type)
+    .then(response=>{
+        res.status(200).send(response);
+    })
+    .catch(error=>{
+        res.status(500).send(error);
+    })
+})
+
+app.get('/getTransactions',(req,res) =>{
+    expenses.getTransactions(req.query.expense_type)
+    .then(response=>{
+        res.status(200).send(response);
+    })
+    .catch(error=>{
+        res.status(500).send(error);
+    })
+})
+
+app.get('/getTotalBudget',(req,res) =>{
+    expenses.getTotalBudget(req.query.expense_type)
+    .then(response=>{
+        res.status(200).send(response);
+    })
+    .catch(error=>{
+        res.status(500).send(error);
+    })
+})
+
+app.delete('/expense/:id',expenses.deleteExpense)
+app.delete('/monthlyExpense/:id',expenses.deleteMonthlyExpense)
+app.delete('/monthlyBudget/:id',expenses.deleteMonthlyBudget)
+
+app.listen(port, () =>{
+    console.log(`App running on port ${port}`)
+})
