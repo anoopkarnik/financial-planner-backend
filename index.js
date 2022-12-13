685a08b43486cc2275e9f486ddf755c7271f4bf0
anoopkarnik <danoopkarnik@gmail.com>
2022-12-13 11:16:26 +0530

    changed link to cors

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const expenses = require('./expenses')
 app.use(express.json())
 
 app.use(function(req,res,next){
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.setHeader('Access-Control-Allow-Origin', 'http://financial-planner.anoopkarnik.net:8080');
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
     next();
